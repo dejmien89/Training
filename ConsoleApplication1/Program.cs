96b463463851fe9a2268beb663ccbcab18e93671
Damian Skorupa <damian.skorupa@atos.net>
2016-05-29 17:31:18 +0200

    Pliki

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> miesiace = new Dictionary<string, int>()
+            {
+                {"Styczeń", 1},
+                {"Luty",2},
+                {"Marzec",3},
+                {"Kwiecień",4},
+                {"Maj",5},
+                {"Czerwiec",6}
+            };
+
+            List<string> miesiaceParzyste = miesiace.Select(x => x.Key).ToList<string>();
+
+            Console.WriteLine("Dziś jest: " + DateTime.Today + (3 - 2 + 7));
+        }
+
+
+    }
+}
